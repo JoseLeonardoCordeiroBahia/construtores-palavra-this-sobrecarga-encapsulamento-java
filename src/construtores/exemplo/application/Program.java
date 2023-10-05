64345e0ba9af65a64ab6b5f5a8bc5a950d983a3c
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-10-05 14:30:20 -0300

    Construtores

---
+++ src/construtores/exemplo/application/Program.java
@@ -0,0 +1,50 @@
+package construtores.exemplo.application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import construtores.exemplo.entities.Product;
+
+public class Program {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter product data: ");
+		
+		System.out.print("Name: ");
+		String name = scanner.nextLine();
+		
+		System.out.print("Price: ");
+		double price = scanner.nextDouble();
+		
+		System.out.print("Quantity in stock: ");
+		int quantity = scanner.nextInt();
+		
+		Product product = new Product(name, price, quantity);
+		
+		System.out.println();
+		System.out.println("Product data: " + product);
+		System.out.println();
+		
+		System.out.print("Enter the number of products to be added in stock: ");
+		quantity = scanner.nextInt();
+		
+		product.addProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Updated data: " + product);
+		System.out.println();
+		
+		System.out.print("Enter the number of products to be removed from stock: ");
+		quantity = scanner.nextInt();
+		
+		product.removeProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Updated data: " + product);
+		
+		scanner.close();
+	}
+}
